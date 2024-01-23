240b25ced126c074352fa148ede23c828f1bc6e4
harhs21 <harshu21122003@gmail.com>
2024-01-24 00:27:53 +0530

    login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
-
-const App=()=> {
-
+const App = () => {
   return (
     <>
-      <Header/>
-      <a></a> 
-      <h1>Home Page</h1>
-      <Footer/>
-    </>
-  )
-}
+      <Header />
+      <div className="container">
+        <div className="top-right-buttons">
+          <Link to="/login"> {/* Use Link instead of <a> */}
+            <div className="scene">
+              <div className="cube">
+                <span className="side top">Sign In</span>
+                <span className="side front">Welcome Back</span>
+              </div>
+            </div>
+          </Link>
 
-export default App
+          {/* Similar Link for Sign Up button */}
+          <Link to="/signup">
+            <div className="scene">
+              <div className="cube">
+                <span className="side top">Sign Up</span>
+                <span className="side front">New User</span>
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <h1>Home Page</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
