722d8340f02ee43c123de92b267a4ec8548a33e5
harhs21 <harshu21122003@gmail.com>
2024-01-24 01:32:21 +0530

    login/signup made

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import React from 'react'; // Import Link from react-router-dom
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css';
 
 const App = () => {
   return (
     <>
       <Header />
       <div className="container">
-        <div className="top-right-buttons">
-          <Link to="/login"> {/* Use Link instead of <a> */}
-            <div className="scene">
-              <div className="cube">
-                <span className="side top">Sign In</span>
-                <span className="side front">Welcome Back</span>
-              </div>
-            </div>
-          </Link>
-
-          {/* Similar Link for Sign Up button */}
-          <Link to="/signup">
-            <div className="scene">
-              <div className="cube">
-                <span className="side top">Sign Up</span>
-                <span className="side front">New User</span>
-              </div>
-            </div>
-          </Link>
-        </div>
-
         <h1>Home Page</h1>
       </div>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './Header.css';
 const Header = ()=>{
       return(
-            <div>
-                  <h1>Header</h1>
+        <div className="top-right-buttons">
+          <Link to="/login"> {/* Use Link instead of <a> */}
+            <div className="scene">
+              <div className="cube">
+                <span className="side top">Welcome Back</span>
+                <span className="side front">Login</span>
+              </div>
             </div>
+          </Link>
+
+          {/* Similar Link for Sign Up button */}
+          <Link to="/signup">
+            <div className="scene">
+              <div className="cube">
+                <span className="side top">New User</span>
+                <span className="side front">Sign Up</span>
+              </div>
+            </div>
+          </Link>
+        </div>     
       )
 }
 export default Header;
